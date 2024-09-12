d243ff5a4d0e142bbb4d825f849e5309c40b6fd4
AdityaX24 <poddar.aditya2014@gmail.com>
2024-09-12 17:42:11 +0530

    Added TransactionContext

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,12 +1,14 @@
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiE, SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 
 import { Loader } from "./";
+import { TransactionContext } from "../context/TransactionContext";
 
 const commonStyles="min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = ({placeholder, name, type, value}) => (
+const Input = ({placeholder, name, type, value, handleChange}) => (
     <input 
     placeholder={placeholder}
     type={type}
@@ -18,12 +20,16 @@ const Input = ({placeholder, name, type, value}) => (
 );
 
 const Welcome = () => {
-    const connectWallet = () => {
+    const { connectWallet,currentAccount, formData, sendTransaction, handleChange} = useContext(TransactionContext);
 
-    }
+    const handleSubmit =(e) => {
+        const { addressTo, amount, keyword, message } = formData;
 
-    const handleSubmit =() => {
+        e.preventDefault();
 
+        if(!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
     }
 
     return ( 
@@ -36,12 +42,13 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the carbon credit World. Buy and sell CCcrypto-currencies easily here.
                     </p>
-                    <button
+                    {!currentAccount && (<button
                     type="button"
                     onClick={connectWallet}
                     className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
                         <p className="text-white text-base text-semibold">Connect Wallet</p>
                     </button>
+                    )}
 
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
@@ -76,10 +83,10 @@ const Welcome = () => {
                         </div>
                     </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}}/>
-                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}}/>
-                        <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={() => {}}/>
-                        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}}/>
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange}/>
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange}/>
+                        <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={handleChange}/>
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange}/>
 
                         <div className="h-[1px] w-full bg-gray-400 my-2"/>
 
